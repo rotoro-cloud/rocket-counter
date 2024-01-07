e70f21f9b1cfee2114ce34548a90237b9a00e46f
Rotoro.cloud <72907533+rotoro-cloud@users.noreply.github.com>
2024-01-07 21:00:40 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,36 @@
+import time
+import random
+import redis
+from flask import Flask, render_template
+
+app = Flask(__name__)
+cache = redis.Redis(host='redis', port=6379)
+
+images = [
+
+    "small.png",
+    "average.png",
+    "big.png"
+
+]
+
+def get_rocket_count():
+    retries = 5
+    while True:
+        try:
+            return cache.incr('rocket')
+        except redis.exceptions.ConnectionError as exc:
+            if retries == 0:
+                raise exc
+            retries -= 1
+            time.sleep(0.5)
+
+@app.route("/getimage")
+def get_img():
+    count = get_rocket_count()
+    image = random.choice(images)
+    return '{' + '"image": "{}", "count": "{}"'.format(image, count) + '}'
+
+@app.route("/")
+def hello():
+    return render_template("img.html")
