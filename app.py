b8d06d47a5f7c61f994459483e1bb34dc387eba6
Rotoro.cloud <72907533+rotoro-cloud@users.noreply.github.com>
2024-03-13 16:17:01 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 import time
 import random
+import os
 import redis
 from flask import Flask, render_template
 
 app = Flask(__name__)
 cache = redis.Redis(host='redis', port=6379)
+
+ROCKET_SIZE = os.environ.get('ROCKET_SIZE') or None
 
 images = [
 
@@ -29,7 +32,10 @@ def get_rocket_count():
 @app.route("/getimage")
 def get_img():
     count = get_rocket_count()
-    image = random.choice(images)
+    if ROCKET_SIZE:
+        image = ROCKET_SIZE + '.png'
+    else:
+        image = random.choice(images)
     return '{' + '"image": "{}", "count": "{}"'.format(image, count) + '}'
 
 @app.route("/")
